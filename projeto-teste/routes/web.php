05df3cda82628c8e17ab663a87d0f464635a5242
unknown <https://github.com/jefferson09-dev/project-laravel.git>
2026-03-17 18:34:00 -0300

    Atualizando projeto

--- projeto-teste/routes/web.php
+++ projeto-teste/routes/web.php
@@ -3,8 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', function () {
-    return view("ulkit.model");
+// Route::get('/', function () {
+//     return view("ulkit.model");
+// });
+
+Route::get('landing_page', function () {
+    return view("welcome");
 });
-
-
